5fbb89407076d39cd48e098d5ac33c955efc0942
ImNotRinka <77236422+RinkaGI@users.noreply.github.com>
2022-07-28 15:21:30 +0200

    Same game but poo

--- examples/pong.py
+++ examples/pong.py
@@ -3,73 +3,82 @@ import random
 
 screen = Gamwo("Pong", 1280, 960)
 
-# Game rectangles
-ball = Entity(screen, "square", 30, 30, screen.width/2 - 15, screen.height/2 - 15, (200, 200, 200))
-player = Entity(screen, "square", 10, 140, screen.width - 20, screen.height/2 - 70, (200, 200, 200))
-opponent = Entity(screen, "square", 10, 140, 10, screen.height/2 - 70, (200, 200, 200))
+class Ball(Entity):
+    def __init__(self, window):
+        super().__init__(window, "circle", 30, 30, window.width/2-15, window.height/2-15, (200, 200, 200))
+        self.ballSpeedX = 7 * random.choice((1, -1))
+        self.ballSpeedY = 7 * random.choice((1, -1))
 
-# Ball code
-ball_speed_x = 7 * random.choice((1, -1))
-ball_speed_y = 7 * random.choice((1, -1))
-# Render entities
+    def movement(self):
+        self.x += self.ballSpeedX
+        self.y += self.ballSpeedY
+
+        if self.top <= 0 or self.bottom >= self.window.height:
+            self.ballSpeedY *= -1
+        if self.left <= 0:
+            self.respawn()
+        elif self.right >= self.window.width:
+            self.respawn()
+
+        if self.checkCollision(player) or self.checkCollision(opponent):
+            self.ballSpeedX *= -1
+
+    def respawn(self):
+        self.center = (self.window.width / 2, self.window.height / 2)
+        self.ballSpeedY *= random.choice((1, -1))
+        self.ballSpeedX *= random.choice((1, -1))
+
+    def render(self):
+        self.show()
+
+class Paddle(Entity):
+    def __init__(self, window, player):
+        if player == 'player':
+            super().__init__(window, "square", 10, 140, window.width - 20, window.height/2 - 70, (200, 200, 200))
+        elif player == 'opponent':
+            super().__init__(window, 'square', 10, 140, 10, window.height/2 - 70, (200, 200, 200))
+
+        self.window = window
+        self.player = player
+
+    def movement(self):
+        if self.player == 'player':
+            if self.window.input.get_key_pressed(self.window.input.keys["UP"]):
+                self.y -= 7
+            if self.window.input.get_key_pressed(self.window.input.keys["DOWN"]):
+                self.y += 7
+        elif self.player == 'opponent':
+            if self.y <= ball.y:
+                self.y += 7
+            elif self.y >= ball.y:
+                self.y -= 7
+
+        
+        if self.top <= 0:
+            self.top = 0
+        if self.bottom >= self.window.height:
+            self.bottom = self.window.height
+        self.show()
+
+ball = Ball(screen)
+player = Paddle(screen, 'player')
+opponent = Paddle(screen, 'opponent')
+
 @screen.draw
 def draw():
     screen.changeBackgroundColor("grey12")
 
     Line(screen, (screen.width/2, 0), (screen.width/2, screen.height), (200, 200, 200))
 
-    ball.show()
+    ball.render()
     player.show()
     opponent.show()
 
-# Update code
 @screen.update
 def update():
-    global ball_speed_x, ball_speed_y
-
-    def ball_restart():
-        global ball_speed_x, ball_speed_y
-        ball.center = (screen.width / 2, screen.height / 2)
-        ball_speed_y *= random.choice((1, -1))
-        ball_speed_x *= random.choice((1, -1))
-
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    if ball.top <= 0 or ball.bottom >= screen.height:
-        ball_speed_y  *= -1
-    if ball.left <= 0:
-        ball_restart()
-    elif ball.right >= screen.width:
-        ball_restart()
-
-    if ball.checkCollision(player) or ball.checkCollision(opponent):
-        ball_speed_x *= -1
-
-    #################################################################3
-
-    if screen.input.get_key_pressed(screen.input.keys["UP"]):
-        player.y -= 7
-
-    if screen.input.get_key_pressed(screen.input.keys["DOWN"]):
-        player.y += 7
-
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= screen.height:
-        player.bottom = screen.height
-
-    ###################################################################33
-
-    if opponent.top < ball.y:
-        opponent.top += 7
-    if opponent.bottom > ball.y:
-        opponent.bottom -= 7
-
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= screen.height:
-        opponent.bottom = screen.height
+    ball.movement()
+    player.movement()
+    opponent.movement()
 
 
-screen.run()
+screen.run() 
