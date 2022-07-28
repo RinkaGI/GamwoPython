59a2e8769871e246d7576eda578d279fb4b427af
ImNotRinka <77236422+RinkaGI@users.noreply.github.com>
2022-07-28 14:18:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "gamwo-py",
+    packages = ['gamwo-py'],
+    version = '0.0.1',
+    description = 'Simple python framework for 2d games',
+    long_description= 'There isnt much to say about this package',
+    long_description_content_type='text/markdown',
+    author='RinkaDev',
+    author_email='rinkadevoficial@gmail.com',
+    url='https://github.com/RinkaGI/Gamwo',
+    keywords=['game', 'framework', '2d', 'simple', 'pygame'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True
+)
