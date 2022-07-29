2daef71c688c70c366daee096becdb2b4feba324
ImNotRinka <77236422+RinkaGI@users.noreply.github.com>
2022-07-29 17:05:25 +0200

    Same game but with new gamwo
    Gamwo was updated adding deltatime

--- examples/pong.py
+++ examples/pong.py
@@ -1,64 +1,63 @@
-from Gamwo import *
+from gamwo import *
 import random
 
-screen = Gamwo("Pong", 1280, 960)
+
+screen = Gamwo("Pong", 800, 600, target_fps=60)
 
 class Ball(Entity):
-    def __init__(self, window):
-        super().__init__(window, "circle", 30, 30, window.width/2-15, window.height/2-15, (200, 200, 200))
-        self.ballSpeedX = 7 * random.choice((1, -1))
-        self.ballSpeedY = 7 * random.choice((1, -1))
+        def __init__(self, window):
+            super().__init__(window, "circle", 30, 30, window.width/2-15, window.height/2-15, (200, 200, 200))
+            self.window = window
 
-    def movement(self):
-        self.x += self.ballSpeedX
-        self.y += self.ballSpeedY
+            self.ballSpeedX = 450 * random.choice((1, -1))
+            self.ballSpeedY = 450 * random.choice((1, -1))
 
-        if self.top <= 0 or self.bottom >= self.window.height:
-            self.ballSpeedY *= -1
-        if self.left <= 0:
-            self.respawn()
-        elif self.right >= self.window.width:
-            self.respawn()
+        def movement(self, dt):
+            self.x += self.ballSpeedX  * dt
+            self.y += self.ballSpeedY  * dt
 
-        if self.checkCollision(player) or self.checkCollision(opponent):
-            self.ballSpeedX *= -1
+            if self.top <= 0 or self.bottom >= self.window.height:
+                self.ballSpeedY *= -1
+            if self.left <= 0:
+                self.respawn()
+            elif self.right >= self.window.width:
+                self.respawn()
 
-    def respawn(self):
-        self.center = (self.window.width / 2, self.window.height / 2)
-        self.ballSpeedY *= random.choice((1, -1))
-        self.ballSpeedX *= random.choice((1, -1))
+            if self.checkCollision(player) or self.checkCollision(opponent):
+                self.ballSpeedX *= -1
 
-    def render(self):
-        self.show()
+
+        def respawn(self):
+            self.center = (self.window.width / 2, self.window.height / 2)
+            self.ballSpeedY *= random.choice((1, -1))
+            self.ballSpeedX *= random.choice((1, -1))
 
 class Paddle(Entity):
-    def __init__(self, window, player):
-        if player == 'player':
-            super().__init__(window, "square", 10, 140, window.width - 20, window.height/2 - 70, (200, 200, 200))
-        elif player == 'opponent':
-            super().__init__(window, 'square', 10, 140, 10, window.height/2 - 70, (200, 200, 200))
+        def __init__(self, window, player):
+            if player == 'player':
+                super().__init__(window, "square", 10, 140, window.width - 20, window.height/2 - 70, (200, 200, 200))
+            elif player == 'opponent':
+                super().__init__(window, 'square', 10, 140, 10, window.height/2 - 70, (200, 200, 200))
 
-        self.window = window
-        self.player = player
+            self.window = window
+            self.player = player
 
-    def movement(self):
-        if self.player == 'player':
-            if self.window.input.get_key_pressed(self.window.input.keys["UP"]):
-                self.y -= 7
-            if self.window.input.get_key_pressed(self.window.input.keys["DOWN"]):
-                self.y += 7
-        elif self.player == 'opponent':
-            if self.y <= ball.y:
-                self.y += 7
-            elif self.y >= ball.y:
-                self.y -= 7
+        def movement(self, dt):
+            if self.player == 'player':
+                if self.window.input.get_key_pressed(self.window.input.keys["UP"]):
+                    self.y -= 450 * dt
+                if self.window.input.get_key_pressed(self.window.input.keys["DOWN"]):
+                    self.y += 450 * dt
+            elif self.player == 'opponent':
+                if self.y <= ball.y:
+                    self.y += 450 * dt
+                elif self.y >= ball.y:
+                    self.y -= 450 * dt
 
-        
-        if self.top <= 0:
-            self.top = 0
-        if self.bottom >= self.window.height:
-            self.bottom = self.window.height
-        self.show()
+            if self.top <= 0:
+                self.top = 0
+            if self.bottom >= self.window.height:
+                self.bottom = self.window.height
 
 ball = Ball(screen)
 player = Paddle(screen, 'player')
@@ -66,19 +65,15 @@ opponent = Paddle(screen, 'opponent')
 
 @screen.draw
 def draw():
-    screen.changeBackgroundColor("grey12")
-
-    Line(screen, (screen.width/2, 0), (screen.width/2, screen.height), (200, 200, 200))
-
-    ball.render()
-    player.show()
-    opponent.show()
+        Text(screen, f"FPS: {str(int(screen.getFPS()))}").show()
+        ball.show()
+        player.show()
+        opponent.show()
 
 @screen.update
-def update():
-    ball.movement()
-    player.movement()
-    opponent.movement()
+def update(dt):
+        ball.movement(dt)
+        player.movement(dt)
+        opponent.movement(dt)
 
-
-screen.run() 
+screen.run()
