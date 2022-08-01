2f5436f06e53d9f5d90e7dd9b368a98565628536
ImNotRinka <77236422+RinkaGI@users.noreply.github.com>
2022-08-01 16:01:08 +0200

    Update __init__.py

--- Gamwo/__init__.py
+++ Gamwo/__init__.py
@@ -3,5 +3,7 @@ from gamwo.entity import Entity
 from gamwo.line import Line
 from gamwo.sprite import Sprite
 from gamwo.text import Text
+from gamwo.timer import Timer
+from gamwo.vector2 import Vector2
 
 __name__ == "Gamwo"
